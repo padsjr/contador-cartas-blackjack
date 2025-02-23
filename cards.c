c82534ec44fb40c3a4481f7d0ff3d005b9ecdd6a
padsjr <pedroadasilveirajr@hotmail.com>
2025-02-22 22:23:34 -0300

    feat contagem up n down
    Implementado verificador para conferir se a contagem sobe ou desce

--- cards.c
+++ cards.c
@@ -18,6 +18,14 @@ int main()
         val = atoi(card_name);
     }
     printf("The card value is: %i\n", val);
+    /* check if the value os 3 to 6*/
+    if ((val >2) && (val < 7))
+        puts("Count has gone up");
+     /* Check if the card was 10, J, Q, or K*/
+     else if((val == 10))
+        puts("Count has gone up");
     return 0;
+
+    
 
 }
