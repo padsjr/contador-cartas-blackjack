6205427bc443cd123292d080773fc001900c0647
padsjr <pedroadasilveirajr@hotmail.com>
2025-03-03 18:24:48 -0300

    Contador de cartas finalizado

--- cards.c
+++ cards.c
@@ -3,31 +3,42 @@
 int main()
 {
     char card_name[3];
-    puts("Enter the card_name: ");
-    scanf("%2s", card_name);
-    int val = 0;
-    switch(card_name[0]){
-        case 'K':
-        case 'Q':
-        case 'K':
-        case 'J':
-            val = 10;
-            break;
-        case 'A':
-            val = 11;
-            break;
-        default:
-            val = atoi(card_name);
-    }
-    printf("The card value is: %i\n", val);
-    /* check if the value os 3 to 6*/
-    if ((val >2) && (val < 7))
-        puts("Count has gone up");
-     /* Check if the card was 10, J, Q, or K*/
-     else if((val == 10))
-        puts("Count has gone up");
+    int count = 0;
+    while(card_name[0] != 'X'){
+        puts("Enter the card_name: ");
+        scanf("%2s", card_name);
+        int val = 0;
+        switch(card_name[0]){
+            case 'K':
+            case 'Q':
+            case 'J':
+                val = 10;
+                break;
+            case 'A':
+                val = 11;
+                break;
+            case 'X':
+                continue;/*Pq nao utilizar o break se ele irá sair igual do loop, pq verificar de novo a condição antes?*/
+            default:
+                val = atoi(card_name);
+                if((val < 1 ) || (val > 10)){
+                    puts("I don't understand that value");
+                }
+                continue;
+        }
+
+        printf("The card value is: %i\n", val);
+        /* check if the value os 3 to 6*/
+        if ((val >2) && (val < 7)){
+            puts("Count has gone up");
+            count ++;
+        }
+        /* Check if the card was 10, J, Q, or K*/
+        else if((val == 10)){
+            puts("Count has gone up");
+            count--;
+        }
+        printf("Current Count: %i\n", count);
+}
     return 0;
-
-    
-
 }
